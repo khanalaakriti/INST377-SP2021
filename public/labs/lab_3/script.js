9354df1ef2e93d549968b51791bdcbdb29b52440
khanalaakriti <66918681+khanalaakriti@users.noreply.github.com>
2021-02-12 15:04:51 -0500

    Completes Lab 3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,36 @@
-/* Put your javascript in here */
+const arrayCarousel = [
+    '<img src="./images/onigiri_1.png" alt="Image 1">',
+    '<img src="./images/onigiri_2.png" alt="Image 2">',
+    '<img src="./images/onigiri_3.png" alt="Image 3">',
+    '<img src="./images/onigiri_4.png" alt="Image 4">',
+    '<img src="./images/roll_1.png" alt="Image 5">',
+    '<img src="./images/roll_2.png" alt="Image 6">',
+    '<img src="./images/roll_3.png" alt="Image 7">'
+];
+
+let currentImageIndex;
+
+window.addEventListener('load', function(event) {
+    currentImageIndex = 0;
+    document.getElementById("carousel").innerHTML = arrayCarousel[currentImageIndex];
+});
+    
+function changeImage(changeByValue){
+    
+    currentImageIndex = currentImageIndex + changeByValue;
+    
+    if (currentImageIndex == 7) {
+        currentImageIndex = 0;
+    }
+
+    if (currentImageIndex == -1) {
+        currentImageIndex = 6;
+    }
+
+    arrayCarousel.map((item, index) => {
+        if (index == currentImageIndex) {
+            document.getElementById("carousel").innerHTML = item;
+        }
+    });
+
+};
