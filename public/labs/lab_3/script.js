b6fd0edaf0374ecd326c30610302e34b47a401f0
khanalaakriti <66918681+khanalaakriti@users.noreply.github.com>
2021-04-08 13:21:59 -0400

    Fixing the carousel

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,36 +1,57 @@
-const arrayCarousel = [
-    '<img src="./images/onigiri_1.png" alt="Image 1">',
-    '<img src="./images/onigiri_2.png" alt="Image 2">',
-    '<img src="./images/onigiri_3.png" alt="Image 3">',
-    '<img src="./images/onigiri_4.png" alt="Image 4">',
-    '<img src="./images/roll_1.png" alt="Image 5">',
-    '<img src="./images/roll_2.png" alt="Image 6">',
-    '<img src="./images/roll_3.png" alt="Image 7">'
-];
+// let currentImageIndex;
+let width = 130;
+let count = 3;
+let list = carousel.querySelector("ul");
+let listElement = carousel.querySelector("li");
+let position = 0;
 
-let currentImageIndex;
-
-window.addEventListener('load', function(event) {
-    currentImageIndex = 0;
-    document.getElementById("carousel").innerHTML = arrayCarousel[currentImageIndex];
-});
-    
-function changeImage(changeByValue){
-    
-    currentImageIndex = currentImageIndex + changeByValue;
-    
-    if (currentImageIndex == 7) {
-        currentImageIndex = 0;
-    }
-
-    if (currentImageIndex == -1) {
-        currentImageIndex = 6;
-    }
-
-    arrayCarousel.map((item, index) => {
-        if (index == currentImageIndex) {
-            document.getElementById("carousel").innerHTML = item;
-        }
-    });
-
+carousel.querySelector(".prev").onclick = function () {
+    position += width * count;
+    position = Math.min(position, 0);
+    list.style.marginLeft = position + 'px';
 };
+
+carousel.querySelector(".next").onclick = function () {
+    position -= width * count;
+    position = Math.max(position, -width * (listElement.length - count));
+    list.style.marginLeft = position + 'px';
+};
+
+function arraysushi() {
+    const iarray = [];
+    const images = document.querySelector('.images');
+    images.forEach((element) => {
+        const image = element.querySelector('li');
+        console.log(element);
+        iarray.append(image);
+    });
+    console.log(iarray);
+};
+arraysushi();
+
+
+
+// window.addEventListener('load', function(event) {
+//     currentImageIndex = 0;
+//     document.getElementById("carousel").innerHTML = arrayCarousel[currentImageIndex];
+// });
+    
+// function changeImage(changeByValue){
+    
+//     currentImageIndex = currentImageIndex + changeByValue;
+    
+//     if (currentImageIndex == 7) {
+//         currentImageIndex = 0;
+//     }
+
+//     if (currentImageIndex == -1) {
+//         currentImageIndex = 6;
+//     }
+
+//     arrayCarousel.map((item, index) => {
+//         if (index == currentImageIndex) {
+//             document.getElementById("carousel").innerHTML = item;
+//         }
+//     });
+
+// };
